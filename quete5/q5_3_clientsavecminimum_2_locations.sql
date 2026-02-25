e7159c5477a8a52107d842b5f01834d649b2349d
mariam <mariamwalamako@gmail.com>
2026-02-25 12:39:14 +0100

    quete5: ajout commentaires HAVING vs WHERE

--- quete5/q5_3_clientsavecminimum_2_locations.sql
+++ quete5/q5_3_clientsavecminimum_2_locations.sql
@@ -1,10 +1,14 @@
--- Clients avec au moins 2 locations
+--clientsavecminimum_2_locations.sql
+-- On regroupe les locations par client
+-- HAVING permet de filtrer après le GROUP BY, donc sur le COUNT
+-- On garde uniquement les clients ayant au moins 2 locations
 SELECT 
     client.nom,
     client.prenom,
-    COUNT(*) AS nombre_locations
+    COUNT() AS nombre_locations
 FROM location
-INNER JOIN client ON location.id_client = client.id_client
+INNER JOIN client 
+        ON location.id_client = client.id_client
 GROUP BY client.id_client, client.nom, client.prenom
-HAVING COUNT(*) >= 2
+HAVING COUNT() >= 2
 ORDER BY nombre_locations DESC;
