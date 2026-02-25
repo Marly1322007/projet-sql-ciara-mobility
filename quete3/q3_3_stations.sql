ca74098bc92b9a84553263471b1d92e58cc686b1
Marly1322007 <marlylaelahouefagenesegrace.dedjiho@ynov.com>
2026-02-25 12:42:37 +0100

    quete3: ajout commentaires explicatifs jointure double station

--- quete3/q3_3_stations.sql
+++ quete3/q3_3_stations.sql
@@ -1,4 +1,8 @@
--- Locations avec station de départ ET d'arrivée
+-- On sélectionne les locations avec leurs stations de départ et d'arrivée.
+-- La station de départ est obligatoire dans le schéma, donc un INNER JOIN suffit.
+-- La station d'arrivée peut être absente (location en cours), donc on utilise un LEFT JOIN.
+-- On joint deux fois la table station grâce à des alias pour distinguer départ et arrivée.
+
 SELECT 
     location.id_location,
     location.date_debut,
@@ -8,5 +12,7 @@ SELECT
     station_arrivee.nom  AS station_arrivee,
     station_arrivee.ville AS ville_arrivee
 FROM location
-INNER JOIN station AS station_depart  ON location.id_station_depart  = station_depart.id_station
-LEFT  JOIN station AS station_arrivee ON location.id_station_arrivee = station_arrivee.id_station;
+INNER JOIN station AS station_depart  
+        ON location.id_station_depart = station_depart.id_station
+LEFT JOIN station AS station_arrivee 
+        ON location.id_station_arrivee = station_arrivee.id_station;
